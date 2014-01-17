c3f675760393d4a3dafc2f64c6080cde2f7962a8
Brett O'Donnell <cornernote@gmail.com>
2014-01-17 10:59:02 +1030

    Update init.lua

--- home_gui/init.lua
+++ home_gui/init.lua
@@ -3,8 +3,8 @@
 Home GUI for Minetest
 
 Copyright (c) 2012 cornernote, Brett O'Donnell <cornernote@gmail.com>
-Source Code: https://github.com/cornernote/minetest-particles
-License: GPLv3
+Source Code: https://github.com/cornernote/minetest-home_gui
+License: BSD-3-Clause https://raw.github.com/cornernote/minetest-home_gui/master/LICENSE
 
 ]]--
 
